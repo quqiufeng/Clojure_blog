c11c376bf435af22977b365def67483b4ce667cf
qiufeng <quqiufeng@gmail.com>
2015-07-31 17:52:06 +0800

    add http-kit support

--- project.clj
+++ project.clj
@@ -12,8 +12,9 @@
                   [ring/ring-defaults "0.1.5"]
                   [ring-basic-authentication "1.0.5"]
                   ]
-  :plugins [[lein-ring "0.9.6"]]
-  :ring {:handler blog.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  ;:plugins [[lein-ring "0.9.6"]]
+  ;:ring {:handler  blog.handler/app}
+
+  :main ^:skip-aot  blog.handler
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- src/blog/handler.clj
+++ src/blog/handler.clj
@@ -7,7 +7,10 @@
             [ring.middleware.defaults :refer :all]
             [blog.models.posts :as posts-model]
             [blog.controllers.posts :as posts-controller]
-            [blog.controllers.admin.posts :as admin-posts-controller]))
+            [org.httpkit.server :as http-kit :refer [run-server]]
+            [blog.controllers.admin.posts :as admin-posts-controller])
+  (:gen-class)
+  )
 
 (defn authenticated? [name pass]
   (and (= name "quqiufeng")
@@ -24,6 +27,7 @@
   (POST "/admin/posts/create" [& params]
     (do (posts-model/create params)
       (resp/redirect "/admin")))
+  (GET "/admin/posts/:id" [id] (admin-posts-controller/show id))
   (GET "/admin/posts/:id/edit" [id] (admin-posts-controller/edit id))
   (POST "/admin/posts/:id/save" [& params]
     (do (posts-model/save (:id params) params)
@@ -37,8 +41,15 @@
   (wrap-basic-authentication protected-routes authenticated?)
   (route/not-found "404 Not Found"))
 
-(def app
+;(def app
  ; (wrap-defaults  app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
-  (wrap-defaults  app-routes site-defaults))
+ ;(wrap-defaults  app-routes site-defaults)
+ ;(run-server (wrap-defaults app-routes site-defaults) {:port 5000})
+ ;)
+
+(defn -main []
+  (run-server (wrap-defaults app-routes site-defaults) {:port 5000})
+  )
+
 
 
